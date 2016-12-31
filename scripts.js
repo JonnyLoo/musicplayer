7beccdd3c69d25d03d6c874ae79447442661caf8
JonnyLoo <jonliu44@gmail.com>
2016-12-30 19:50:00 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function changeSong() {
+	if(document.getElementById("song1").src == "https://embed.spotify.com/?uri=spotify:track:0lYBSQXN6rCTvUZvg9S0lU") {
+		document.getElementById("song1").src = "https://embed.spotify.com/?uri=spotify:track:7zsXy7vlHdItvUSH8EwQss";
+	}
+	else
+		document.getElementById("song1").src = "https://embed.spotify.com/?uri=spotify:track:0lYBSQXN6rCTvUZvg9S0lU";
+}
+
+function changeSong2() {
+	if(document.getElementById("song2").src == "https://embed.spotify.com/?uri=spotify:track:0AKejOKlGdiB53QpwAeenO") {
+		document.getElementById("song2").src = "https://embed.spotify.com/?uri=spotify:track:6YgV0EF8xJMZz0Zm6BTaT4";
+	}
+	else
+		document.getElementById("song2").src = "https://embed.spotify.com/?uri=spotify:track:0AKejOKlGdiB53QpwAeenO";
+}
+
+function changeSong3() {
+	if(document.getElementById("song3").src == "https://embed.spotify.com/?uri=spotify:track:6NMNgWgEAzde5M8U3lc6FN") {
+		document.getElementById("song3").src = "https://embed.spotify.com/?uri=spotify:track:12VWzyPDBCc8fqeWCAfNwR";
+	}
+	else
+		document.getElementById("song3").src = "https://embed.spotify.com/?uri=spotify:track:6NMNgWgEAzde5M8U3lc6FN";
+}
